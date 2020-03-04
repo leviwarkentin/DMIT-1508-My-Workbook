908d0466e525ff94001e2bce33facb9e2bc5f388
leviwarkentin <56049746+leviwarkentin@users.noreply.github.com>
2020-03-04 15:52:28 -0700

    Update A-Insert.sql

--- DML/A-Insert.sql
+++ DML/A-Insert.sql
@@ -15,6 +15,12 @@ GO -- Execute the code up to this point as a single batch
     INSERT INTO TableName(Comma, Separated, ListOf, ColumnNames)
     VALUES ('A', 'Value', 'Per', 'Column'),
            ('Another', 'Row', 'Of', 'Values')
+
+	When inserting values, you can use a subqueries for individual values
+	provided that the subquery returns a single value:
+
+	INSERT INTO TableName(Comma, Seperated, ListOf, ColumnNames)
+	VALUES ('A', (SELECT SignleValue FROM SomeTable), 'Per', 'Column')
     
     Another syntax for the INSERT statement is to use a SELECT clause in place
     of the VALUES clause. This is used for zero-to-many possible rows to insert.
@@ -48,6 +54,12 @@ FROM    Position
 WHERE   PositionID NOT IN (SELECT PositionID FROM Staff)
 --      Add Sheldon Murray as the new Assistant Dean.
 -- TODO: Student Answer Here....
+INSERT INTO Staff(FirstName, LastName, DateHired, PositionID)
+VALUES ('Sheldon', 'Murray', GETDATE(), 
+        (SELECT PositionID
+        FROM   Position
+        WHERE  PositionDescription = 'Assistant Dean'))
+-- SELECT * FROM Staff
 
 -- 3. There are three additional clubs being started at the school:
 --      - START - Small Tech And Research Teams
@@ -63,7 +75,12 @@ VALUES ('START', 'Small Tech And Research Teams'),
 -- 4. In your web browser, use https://randomuser.me/ to get information on three
 --    people to add as new students. Write separate insert statement for each new student.
 -- TODO: Student Answer Here....
-
+-- TIP: When inserting into a datetime column, you can use a string and SQL Server
+-- will convert it for you.  E.g.: 'Jan 5, 2020'
+-- SP_HELP Student
+INSERT INTO Student(FirstName, LastName, Gender, StreetAddress, City, Province, PostalCode, Birthdate)
+VALUES ('Katrina', 'White', 'f', '5074 E Center St', 'LLB', 'AB', 'T0A2C1', 'Jan 5, 1943')
+SELECT * FROM Student
 
 -- 5. Enroll each of the students you've added into the DMIT777 course.
 --    Use 'Dan Gilleland' as the instructor. At this point, their marks should be NULL.
